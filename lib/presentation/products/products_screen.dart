eec330944a4d378f165c5c2c5c97008b593a8181
DANILO_PH <150541746+danilo674220@users.noreply.github.com>
2025-01-20 21:27:14 -0600

    Update products_screen.dart

--- lib/presentation/products/products_screen.dart
+++ lib/presentation/products/products_screen.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProductPage(),
-    );
-  }
-}
-
 class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
